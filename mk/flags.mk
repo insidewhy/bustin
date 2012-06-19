b4a814da80fb87b9810210b77d1f813cbe925d5e
jpike <jpike@chilon.net>
2012-06-19 22:36:18 +0100

    llvm-3.1 is out

--- mk/flags.mk
+++ mk/flags.mk
@@ -1,3 +1,3 @@
 # dmd only supports static libraries directly so just pass the linker flags
 # through.. this works on linux at least
-llvm_link = -L-L/usr/lib/llvm -L-lLLVM-3.0 -L-lstdc++ -L-ldl
+llvm_link = -L-L/usr/lib/llvm -L-lLLVM-3.1 -L-lstdc++ -L-ldl
